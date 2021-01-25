c2d920b7f0b7fe8fa6a3a125834c2946c9e8d9f9
TheoOC <61147292+TheoOC@users.noreply.github.com>
2021-01-25 16:16:26 +0100

    added default-admin seed wich created a create a admin user on up and delete all users on down

---
+++ backend/seeders/20210125142641-default-admin.js
@@ -0,0 +1,26 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    //need to think about moving bcrypt.hash to sequelize models hooks or not  
+    
+    //password of default administrator
+    let hashedPassword = `qwerty`;
+    await bcrypt.hash(`qwerty`, 10).then((hash) => { hashedPassword = hash }).catch((error) => { console.log(`hash failed: ${error}`) });
+
+    return queryInterface.bulkInsert('Users', [{
+      email: `admin@gmail.com`,
+      password: `${hashedPassword}`,
+      username: `administrator`,
+      isAdmin: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    //remove all users
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
